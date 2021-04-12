b61571e7445c3adc956b490bfa23a6e36c9cb591
Sylpha777 <sylpha.ponrena@gmail.com>
2021-04-12 02:45:47 +0000

    user login

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,10 @@ Rails.application.routes.draw do
   root to: 'tasks#index'
   resources :tasks
   
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+  
   get 'signup', to: 'users#new'
   post 'users', to: 'users#create'
   
